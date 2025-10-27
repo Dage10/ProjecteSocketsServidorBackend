4e2a53d4dabd7c9a51484dc1a13cd8e58a934618
Dage10 <davidgelmacorral@gmail.com>
2025-10-27 09:09:35 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: '*' }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
