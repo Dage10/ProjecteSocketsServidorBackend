d47831a09dfdf18ffaf86a54f3dd92e915ab787f
Dage10 <davidgelmacorral@gmail.com>
2025-11-03 08:50:10 +0100

    Endpoints validar codigo,funcion generar codigo y connection inicial

--- server.js
+++ server.js
@@ -2,16 +2,55 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: '*' }
 });
+app.use('/videos', express.static(path.join(__dirname, 'videos')));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+const videos = [
+    {id: 1, nombre_archivo:'video1.mp4', codigo: null,permitido:false},
+    {id: 2, nombre_archivo: 'video2.mp4', codigo: null,permitido:false}
+];
+
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado', socket.id);
+
+    socket.on('pedirVideo', () => {
+        const video = videos[Math.floor(Math.random() * videos.length)];
+        video.codigo = generarCodigo();
+        socket.emit('videoAsignado', { id: video.id, nombre_archivo: video.nombre_archivo, codigo: video.codigo, url: `/videos/${video.nombre_archivo}` });
+    });
+});
+
+function generarCodigo() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let codigo = '';
+    for (let i = 0; i < 4; i++) {
+        codigo += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return codigo;
+}
+
+app.post('/validarCodigo', (req, res) => {
+    const { id, codigo } = req.body;
+    const video = videos.find(v => v.id === id);
+    if (!video) return res.status(404).json({ ok: false, mensaje: 'Video no encontrado' });
+
+    if (video.codigo === codigo) {
+        return res.json({ ok: true, mensaje: 'Codi correcto, reproduccion permitida' });
+    } else {
+        return res.json({ ok: false, mensaje: 'Codigo incorrecto' });
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('Client connectat:', socket.id);
